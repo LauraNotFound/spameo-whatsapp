c3985f7fd3f95bd72a0305f16414412ac158d7cc
LAURA CECILIA MENDOZA MORALES <lcmm97.lcmm@gmail.com>
2024-04-27 21:09:00 -0500

    Agregar script send_whats en python

---
+++ send_whats.py
@@ -0,0 +1,19 @@
+import pywhatkit
+from number import mobile
+from number import mobiles
+
+msg = ""
+time_hour = 8
+time_min = 27
+for i in mobile:
+
+    pywhatkit.sendwhatmsg(i,msg, time_hour, time_min, 32, True, 3)
+    time_min = time_min + 1
+    if (time_min >= 60 ):
+        time_hour = time_hour + 1
+        time_min = 0
+
+
+
+
+# msg = "¿Te apasiona la tecnología y quieres aprender a crear aplicaciones web con Python? \n Este webinar gratuito está especialmente diseñado para chicas de la FISI de todas las especialidades.\nEn este taller aprenderás: Fundamentos de Python, Desarrollo web con Django y APIs con Django REST Framework.\nNo necesitas experiencia previa en Python, aquí te guiaremos desde cero.\nFechas: Sábados 6, 13, 20 y 27 de abril.\nHora: 06:00 p.m.\nLugar: Laboratorio de la FISI (por asignar) y vía Zoom para quienes no puedan asistir de forma presencial.\nInscripciones:  https://forms.gle/hZ57ipwP1A7Zapo56 \nInstructor: Jorge Jesús de la Base 22\nSi ya te inscribiste omite el mensaje, por favor. Gracias."
